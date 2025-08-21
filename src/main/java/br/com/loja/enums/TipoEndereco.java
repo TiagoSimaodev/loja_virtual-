e4ac8d5db01f1e02d584ac512b0236941f7d71bd
tiago simao <tiagosimaorodri123@gmail.com>
2025-08-21 02:12:03 -0300

    criado o enum de endereço,

---
+++ src/main/java/br/com/loja/enums/TipoEndereco.java
@@ -0,0 +1,25 @@
+package br.com.loja.enums;
+
+public enum TipoEndereco {
+	
+	COBRANCA("Cobrança"),
+	ENTREGA("Entrega");
+	
+	private String descricao;
+	
+	private TipoEndereco(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	@Override
+	public String toString() {
+	
+		return this.descricao;
+	}
+	
+	
+}
