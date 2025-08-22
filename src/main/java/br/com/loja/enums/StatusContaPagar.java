53628a8498508842f07b4f9d36f9c685f03d8e41
tiago simao <tiagosimaorodri123@gmail.com>
2025-08-22 00:50:21 -0300

    criado as classes de status da conta a pagar e conta a receber.

---
+++ src/main/java/br/com/loja/enums/StatusContaPagar.java
@@ -0,0 +1,27 @@
+package br.com.loja.enums;
+
+public enum StatusContaPagar {
+	
+	COBRANCA("Pagar"),
+	VENCIDA("Vencida"),
+	ABERTA("Aberta"),
+	QUITADA("Quitada"),
+	NEGOCIADA("Renegociada");
+	
+	
+	private String descricao;
+	
+	private StatusContaPagar(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	@Override
+	public String toString() {
+		return this.descricao;
+	}
+	
+}
