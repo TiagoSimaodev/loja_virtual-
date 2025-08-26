a0538717c9b89fe47485198b599804bd7fa60e62
tiago simao <tiagosimaorodri123@gmail.com>
2025-08-26 01:56:19 -0300

    feito alterações essenciais com anotações

--- src/main/java/br/com/loja/LojaVirtualApplication.java
+++ src/main/java/br/com/loja/LojaVirtualApplication.java
@@ -3,9 +3,15 @@ package br.com.loja;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EntityScan(basePackages = "br.com.loja.model")
+@ComponentScan(basePackages = {"br.*"})
+@EnableJpaRepositories(basePackages = {"br.com.loja.repository"})
+@EnableTransactionManagement
 public class LojaVirtualApplication {
 
 	public static void main(String[] args) {
