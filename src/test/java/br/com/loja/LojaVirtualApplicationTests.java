709ce47dae636da59c6fb11be544b5b40c4eb81f
tiago simao <tiagosimaorodri123@gmail.com>
2025-08-26 01:56:51 -0300

    feito o teste de cadastro de um usuario ROLE, para verificar se está tudo funcionando.

--- src/test/java/br/com/loja/LojaVirtualApplicationTests.java
+++ src/test/java/br/com/loja/LojaVirtualApplicationTests.java
@@ -1,13 +1,34 @@
 package br.com.loja;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class LojaVirtualApplicationTests {
+import br.com.loja.controller.AcessoController;
+import br.com.loja.model.Acesso;
+import br.com.loja.repository.AcessoRepository;
+import br.com.loja.service.AcessoService;
 
+@SpringBootTest(classes = LojaVirtualApplication.class)
+public class LojaVirtualApplicationTests {
+	
+	@Autowired
+	private AcessoService acessoService;
+
+	@Autowired
+	private AcessoController acessoController;
+	
+	//@Autowired
+//	private AcessoRepository acessoRepository;
+	
+	
 	@Test
-	void contextLoads() {
+	public void testCadastraAcesso() {
+		Acesso acesso = new Acesso();
+		acesso.setDescricao("ROLE_ADMIN");
+		
+		acessoController.salvarAcesso(acesso);
+		
 	}
 
 }
