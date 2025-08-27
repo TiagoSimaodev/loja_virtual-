13080cc2c31f91074de18261d2c0bbbda94237ec
tiago simao <tiagosimaorodri123@gmail.com>
2025-08-26 21:48:29 -0300

    criado o processo da construção do spring security, e feita algumas alterações no controller e no repository

--- src/main/java/br/com/loja/controller/AcessoController.java
+++ src/main/java/br/com/loja/controller/AcessoController.java
@@ -1,22 +1,50 @@
 package br.com.loja.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.loja.model.Acesso;
+import br.com.loja.repository.AcessoRepository;
 import br.com.loja.service.AcessoService;
 
 @Controller
+@RestController
 public class AcessoController {
 
 	@Autowired
 	private AcessoService acessoService;
 	
-	@PostMapping("/salvarAcesso")
-	public Acesso salvarAcesso(Acesso acesso) {
-		return acessoService.save(acesso);
+	@Autowired
+	private AcessoRepository acessoRepository;
+	
+	
+	@ResponseBody // Poder dar um retorno da API
+	@PostMapping(value = "**/salvarAcesso") // mapeando a url para receber JSON
+	public ResponseEntity<Acesso> salvarAcesso(@RequestBody Acesso acesso) {//Recebe o json e converte para objeto
+		
+		Acesso acessoSalvo = acessoService.save(acesso);
+		
+		return new ResponseEntity<Acesso>(acessoSalvo, HttpStatus.OK);
 	}
+	
+	@ResponseBody
+	@PostMapping(value ="**/deleteAcesso")
+	public ResponseEntity<?> deleteAcesso(@RequestBody Acesso acesso){
+		acessoRepository.deleteById(acesso.getId());
+		
+		return new ResponseEntity(HttpStatus.OK);
+		
+		
+	}
+	
+	
 	
 	
 }
