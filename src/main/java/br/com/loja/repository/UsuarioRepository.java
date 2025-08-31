718202637b6f52ee17f5cd3131e1202d4a2b500e
tiago simao <tiagosimaorodri123@gmail.com>
2025-08-31 16:15:27 -0300

    feita a configuração e adicionada as dependencias do spring security com jwt e criado a classe repository do usuario

---
+++ src/main/java/br/com/loja/repository/UsuarioRepository.java
@@ -0,0 +1,17 @@
+package br.com.loja.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.loja.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+	
+	//para consultar o usuario no banco.
+	@Query("select u from Usuario u where u.login = ?1")
+	Usuario findUserByLogin(String login);
+	
+	
+}
