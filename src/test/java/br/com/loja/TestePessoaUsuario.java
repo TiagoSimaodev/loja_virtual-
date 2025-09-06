72858d76ca52568f2b0fa77feef68c541d3752d1
tiago simao <tiagosimaorodri123@gmail.com>
2025-09-06 02:57:29 -0300

    feito o teste unitário para salvar, tentando o controler tanto com os teste unitario, quanto no postman

--- src/test/java/br/com/loja/TestePessoaUsuario.java
+++ src/test/java/br/com/loja/TestePessoaUsuario.java
@@ -1,9 +1,12 @@
 package br.com.loja;
 
+import java.util.Calendar;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import br.com.loja.controller.PessoaController;
 import br.com.loja.model.PessoaFisica;
 import br.com.loja.model.PessoaJuridica;
 import br.com.loja.repository.PessoaRepository;
@@ -13,40 +16,27 @@ import junit.framework.TestCase;
 @SpringBootTest(classes = LojaVirtualApplication.class)
 public class TestePessoaUsuario extends TestCase {
 
-	@Autowired
-	private PessoaUserService pessoaUserService; 
+	
 	
 	@Autowired
-	private PessoaRepository pessoaRepository;
+	private PessoaController pessoaController;
 	
 	@Test
-	public void testeCadPessoaFisica() {
+	public void testeCadPessoaFisica() throws ExceptionLojaVirtual {
 		
 		PessoaJuridica pessoaJuridica = new PessoaJuridica();
 		
-		pessoaJuridica.setCnpj("32123221231313110");
+		pessoaJuridica.setCnpj("" + Calendar.getInstance().getTimeInMillis());
 		pessoaJuridica.setNome("Tiago");
-		pessoaJuridica.setEmail("tiago@gmail.com");
+		pessoaJuridica.setEmail("tiagoTESTEPJ@gmail.com");
 		pessoaJuridica.setTelefone("9999919191");
 		pessoaJuridica.setInscrEstadual("9191831831919");
 		pessoaJuridica.setInscrMunicipal("39391031010");
 		pessoaJuridica.setNomeFantasia("101391391910310");
 		pessoaJuridica.setRazaoSocial("1091991393910");
+		
+		pessoaController.salvarPj(pessoaJuridica);
 	
-		pessoaRepository.save(pessoaJuridica);
-		
-		
-		/*
-		PessoaFisica pessoaFisica = new PessoaFisica();
-		
-		pessoaFisica.setCpf("32123221230");
-		pessoaFisica.setNome("Tiago");
-		pessoaFisica.setEmail("tiago@gmail.com");
-		pessoaFisica.setTelefone("9999919191");
-		pessoaFisica.setEmpresa(pessoaFisica);		
-		*/
-		
+	
 	}
-	
-	
 }
