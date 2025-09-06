377e338d638aa8b0ab4170f7ecf70b9f94c29230
tiago simao <tiagosimaorodri123@gmail.com>
2025-09-06 02:54:15 -0300

    criado o metodo de salvar uma pessoajuridica, e feito toda a regra de negocio. tanto na service como no controller

---
+++ src/main/java/br/com/loja/controller/PessoaController.java
@@ -0,0 +1,44 @@
+package br.com.loja.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.loja.ExceptionLojaVirtual;
+import br.com.loja.model.PessoaJuridica;
+import br.com.loja.repository.PessoaRepository;
+import br.com.loja.service.PessoaUserService;
+
+@RestController
+public class PessoaController {
+
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	@Autowired
+	private PessoaUserService pessoaUserService;
+	
+	//endpoint é microservicoe é um api
+	@ResponseBody
+	@PostMapping(value = "**/salvarPj")
+	public ResponseEntity<PessoaJuridica> salvarPj(@RequestBody PessoaJuridica pessoaJuridica) throws ExceptionLojaVirtual {
+		
+		if (pessoaJuridica == null) {
+			throw new ExceptionLojaVirtual("Pessoa juridica não pode ser Null");
+		}
+		
+		if(pessoaJuridica.getId() == null && pessoaRepository.existeCnpjCadastrado(pessoaJuridica.getCnpj()) != null ) {
+			throw new ExceptionLojaVirtual("Já existe um CNPJ cadastrado com o numero: " + pessoaJuridica.getCnpj());
+		}
+		
+		pessoaJuridica = pessoaUserService.salvarPessoaJuridica(pessoaJuridica);
+		
+		return new ResponseEntity<PessoaJuridica>(pessoaJuridica, HttpStatus.OK);
+	}
+	
+}
--- src/main/java/br/com/loja/repository/PessoaRepository.java
+++ src/main/java/br/com/loja/repository/PessoaRepository.java
@@ -1,5 +1,6 @@
 package br.com.loja.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,9 @@ import br.com.loja.model.PessoaJuridica;
 @Repository
 public interface PessoaRepository extends CrudRepository<PessoaJuridica, Long> {
 
+	
+	@Query(value = "select pj from PessoaJuridica pj where pj.cnpj = ?1" )
+	public PessoaJuridica existeCnpjCadastrado(String cnpj);
+	
+	
 }
