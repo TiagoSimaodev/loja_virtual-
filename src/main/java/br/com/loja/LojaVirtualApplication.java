0b6a53280fac9192fd839cf98c7a0eac37e985b0
tiago simao <tiagosimaorodri123@gmail.com>
2025-09-21 19:32:54 -0300

    apenas correção.

--- src/main/java/br/com/loja/LojaVirtualApplication.java
+++ src/main/java/br/com/loja/LojaVirtualApplication.java
@@ -26,6 +26,7 @@ public class LojaVirtualApplication implements AsyncConfigurer {
 	public static void main(String[] args) {
 		SpringApplication.run(LojaVirtualApplication.class, args);
 	
+		
 	System.out.println(new BCryptPasswordEncoder().encode("123"));
 	
 	
