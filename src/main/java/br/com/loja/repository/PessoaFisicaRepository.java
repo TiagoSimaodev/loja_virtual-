ea1103551f8ed3d0bf4ea13467579473bdf2b133
tiago simao <tiagosimaorodri123@gmail.com>
2025-09-21 19:32:19 -0300

    adicionado as query de consulta no repository PF

--- src/main/java/br/com/loja/repository/PessoaFisicaRepository.java
+++ src/main/java/br/com/loja/repository/PessoaFisicaRepository.java
@@ -1,12 +1,25 @@
 package br.com.loja.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.loja.model.PessoaFisica;
+import br.com.loja.model.PessoaJuridica;
 
 @Repository
 public interface PessoaFisicaRepository extends CrudRepository<PessoaFisica, Long> {
+	
+	@Query(value = "select pf from PessoaFisica pf where upper(trim(pf.nome)) like %?1%" )
+	public List<PessoaFisica> pesquisaPorNomePf(String nome);
+	
+	
+	@Query(value = "select pf from PessoaFisica pf where pf.cpf = ?1" )
+	public List<PessoaFisica> pesquisaPorCpfPf(String cpf);
+	
+	
 	
 	
 }
