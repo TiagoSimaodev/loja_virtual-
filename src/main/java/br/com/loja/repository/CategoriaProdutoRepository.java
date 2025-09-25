b5bc422a15e5772f4df1e7e272168e38ad44267e
tiago simao <tiagosimaorodri123@gmail.com>
2025-09-25 02:01:02 -0300

    feita as query de validação no repository, verificando se existe a nivel banco, com native query

--- src/main/java/br/com/loja/repository/CategoriaProdutoRepository.java
+++ src/main/java/br/com/loja/repository/CategoriaProdutoRepository.java
@@ -1,11 +1,21 @@
 package br.com.loja.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.loja.model.CategoriaProduto;
 
 @Repository
 public interface CategoriaProdutoRepository extends JpaRepository<CategoriaProduto, Long> {
+	
+	@Query(nativeQuery = true, value = "select count(1) > 0 from categoria_produto where upper(trim(nome_desc)) = upper(trim(?1))")
+	public boolean existeCategoria(String nomeCategoria);
 
+	@Query("select c from CategoriaProduto c where upper(trim(c.nomeDesc)) like %?1%")
+	public List<CategoriaProduto> buscarCategoriDesc(String nomeDesc);
+	
+	
 }
