7286e77be0a9922af7ffadfc0e5a1e5e586c9088
tiago simao <tiagosimaorodri123@gmail.com>
2025-09-27 01:47:42 -0300

    criado uma nova query para buscar por nome e por id a empresa.

--- src/main/java/br/com/loja/repository/ProdutoRepository.java
+++ src/main/java/br/com/loja/repository/ProdutoRepository.java
@@ -14,9 +14,14 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 	
 	@Query(nativeQuery = true, value = "select count(1) > 0 from produto where upper(trim(nome)) = upper(trim(?1))")
 	public boolean existeProduto(String produto);
+	
+	@Query(nativeQuery = true, value = "select count(1) > 0 from produto where upper(trim(nome)) = upper(trim(?1)) and empresa_id = ?2")
+	public boolean existeProduto(String produto, Long idEmpresa);
 
 	@Query("select p from Produto p where upper(trim(p.nome)) like %?1%")
 	public List<Produto> buscarProdutoNome(String nome);
 	
+	@Query("select p from Produto p where upper(trim(p.nome)) like %?1%and p.empresa.id = ?2")
+	public List<Produto> buscarProdutoNome(String nome, Long id);
 	
 }
