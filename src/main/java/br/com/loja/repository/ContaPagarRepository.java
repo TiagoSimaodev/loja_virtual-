705a1166b58c16997e0123a23ad9c159733b84bf
tiago simao <tiagosimaorodri123@gmail.com>
2025-09-28 04:12:00 -0300

    criado o repository e as query para as busca

---
+++ src/main/java/br/com/loja/repository/ContaPagarRepository.java
@@ -0,0 +1,30 @@
+package br.com.loja.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.loja.model.ContaPagar;
+
+@Repository
+@Transactional
+public interface ContaPagarRepository extends JpaRepository<ContaPagar, Long>   {
+
+	@Query("select a from ContaPagar a where upper(trim(a.descricao)) like %?1%")
+	List<ContaPagar> buscaContaDesc(String desc);
+	
+	@Query("select a from ContaPagar a where a.pessoa.id = ?1")
+	List<ContaPagar> buscaContaPorPessoa(Long idPessoa);
+	
+	@Query("select a from ContaPagar a where a.pessoa_fornecedor = ?1")
+	List<ContaPagar> buscaContaPorFornecedor(Long idFornecedor);
+	
+	@Query("select a from ContaPagar a where a.empresa = ?1")
+	List<ContaPagar> buscaContaPorEmpresa(Long idEmpresa);
+	
+
+}
+
