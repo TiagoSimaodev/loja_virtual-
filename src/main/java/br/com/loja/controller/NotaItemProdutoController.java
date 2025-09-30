058cffdc717e57513c24111b49a1301754e60878
tiago simao <tiagosimaorodri123@gmail.com>
2025-09-30 01:39:42 -0300

    iniciado o crud para salvar o nota item, iniciado o salvar e fazendo as validações com anotações e a nivel de código.

---
+++ src/main/java/br/com/loja/controller/NotaItemProdutoController.java
@@ -0,0 +1,64 @@
+package br.com.loja.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.loja.ExceptionLojaVirtual;
+import br.com.loja.model.NotaItemProduto;
+import br.com.loja.repository.NotaItemProdutoRepository;
+
+@RestController
+public class NotaItemProdutoController {
+
+	@Autowired
+	private NotaItemProdutoRepository notaItemProdutoRepository;
+	
+	@ResponseBody
+	@PostMapping(value = "**/salvarNotaItemProduto")
+	public ResponseEntity<NotaItemProduto> salvarNotaItemProduto(@RequestBody @Valid NotaItemProduto notaItemProduto) throws ExceptionLojaVirtual {
+		
+		if(notaItemProduto.getId() == null) {
+			
+			if(notaItemProduto.getProduto() == null || notaItemProduto.getProduto().getId() <= 0 ) {
+				throw new ExceptionLojaVirtual("Produto deve ser informado.");
+			}
+			
+			if(notaItemProduto.getNotaFiscalCompra() == null || notaItemProduto.getNotaFiscalCompra().getId() <= 0) {
+				throw new ExceptionLojaVirtual("Nota fiscal deve ser informada.");
+			}
+			
+			if(notaItemProduto.getEmpresa() == null || notaItemProduto.getEmpresa().getId() <= 0) {
+				throw new ExceptionLojaVirtual("A empresa deve ser informada.");
+			}
+			
+			List<NotaItemProduto> notaExistente = notaItemProdutoRepository.buscaNotaItemPorProdutoNota(notaItemProduto.getProduto().getId(), notaItemProduto.getNotaFiscalCompra().getId());
+		
+			
+			if(!notaExistente.isEmpty()) {
+				throw new ExceptionLojaVirtual("Já existe este produto cadastrado para esta nota");
+			}
+		
+		}
+		
+		if (notaItemProduto.getQuantidade() <= 0) {
+			throw new ExceptionLojaVirtual("A quantidade do produto deve ser informada");
+		}
+		
+		
+		NotaItemProduto notaItemProdutoSalvo = notaItemProdutoRepository.save(notaItemProduto);
+		
+		
+		return new ResponseEntity<NotaItemProduto>(notaItemProdutoSalvo, HttpStatus.OK);
+	}
+	
+	
+}
