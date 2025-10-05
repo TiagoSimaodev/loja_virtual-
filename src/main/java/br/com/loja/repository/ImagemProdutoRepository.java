f0c9e1614ff12e550ef0eb762f34e754bd62d41e
tiago simao <tiagosimaorodri123@gmail.com>
2025-10-05 02:54:14 -0300

    Criado a classe repositoru da imagem do produto e feito as query para a regra de negocio.

---
+++ src/main/java/br/com/loja/repository/ImagemProdutoRepository.java
@@ -0,0 +1,22 @@
+package br.com.loja.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.loja.model.ImagemProduto;
+
+@Repository
+@Transactional
+public interface ImagemProdutoRepository extends JpaRepository<ImagemProduto, Long> {
+	
+	@Query("select a from ImagemProduto a where a.produto.id = ?1")
+	List<ImagemProduto> buscaImagemProduto(Long idProduto);
+	
+	@Query(nativeQuery = true, value = "delete from imagem_produto where produto_id = ?1")
+	void deleteImagens(Long idProduto);
+	
+}
