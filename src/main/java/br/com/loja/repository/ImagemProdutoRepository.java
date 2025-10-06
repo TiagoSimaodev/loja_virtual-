ad59ceed9afbd9dcf06c8a7683df05bb9c4ffe11
tiago simao <tiagosimaorodri123@gmail.com>
2025-10-06 00:01:43 -0300

    feito a classe de repository e algumas querys especificas.

--- src/main/java/br/com/loja/repository/ImagemProdutoRepository.java
+++ src/main/java/br/com/loja/repository/ImagemProdutoRepository.java
@@ -3,6 +3,7 @@ package br.com.loja.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +17,8 @@ public interface ImagemProdutoRepository extends JpaRepository<ImagemProduto, Lo
 	@Query("select a from ImagemProduto a where a.produto.id = ?1")
 	List<ImagemProduto> buscaImagemProduto(Long idProduto);
 	
+	@Transactional
+	@Modifying(flushAutomatically = true)
 	@Query(nativeQuery = true, value = "delete from imagem_produto where produto_id = ?1")
 	void deleteImagens(Long idProduto);
 	
