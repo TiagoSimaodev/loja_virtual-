b2c8737b92713e5c089582450997c5a26478c65b
tiago simao <tiagosimaorodri123@gmail.com>
2025-10-09 01:32:27 -0300

    criado o repository da classe vendaCompraLoja inicial

---
+++ src/main/java/br/com/loja/repository/Vd_Cp_loja_virtual_repository.java
@@ -0,0 +1,16 @@
+package br.com.loja.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.loja.model.VendaCompraLojaVirtual;
+
+@Repository
+@Transactional
+public interface Vd_Cp_loja_virtual_repository extends JpaRepository<VendaCompraLojaVirtual, Long> {
+
+	
+	
+	
+}
