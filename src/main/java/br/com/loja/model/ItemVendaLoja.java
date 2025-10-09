6a59b7cdffd72c51151ae04cc2507c7ad646f38f
tiago simao <tiagosimaorodri123@gmail.com>
2025-10-09 01:30:23 -0300

    feita algumas alterações, para correções de bug da pessoaJuridica

--- src/main/java/br/com/loja/model/ItemVendaLoja.java
+++ src/main/java/br/com/loja/model/ItemVendaLoja.java
@@ -37,17 +37,17 @@ public class ItemVendaLoja implements Serializable {
 	@Column(nullable = false)
 	private Double quantidade;
 	
-	@ManyToOne(targetEntity = Pessoa.class)
+	@ManyToOne(targetEntity = PessoaJuridica.class)
 	@JoinColumn(name = "empresa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "empresa_id_fk"))
-	private Pessoa empresa;
+	private PessoaJuridica empresa;
 	
 	
 
-	public Pessoa getEmpresa() {
+	public PessoaJuridica getEmpresa() {
 		return empresa;
 	}
 
-	public void setEmpresa(Pessoa empresa) {
+	public void setEmpresa(PessoaJuridica empresa) {
 		this.empresa = empresa;
 	}
 
