202973c8f9b22de1811e709161e1dc60d40027b9
tiago simao <tiagosimaorodri123@gmail.com>
2025-10-13 00:34:31 -0300

    feita alteração na classe controller, para setar a pessoa fisica.

--- src/main/java/br/com/loja/controller/Vd_Cp_Loja_Virt_Controller.java
+++ src/main/java/br/com/loja/controller/Vd_Cp_Loja_Virt_Controller.java
@@ -10,9 +10,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.loja.ExceptionLojaVirtual;
 import br.com.loja.model.Endereco;
+import br.com.loja.model.PessoaFisica;
 import br.com.loja.model.VendaCompraLojaVirtual;
 import br.com.loja.repository.EnderecoRepository;
+import br.com.loja.repository.PessoaFisicaRepository;
 import br.com.loja.repository.Vd_Cp_loja_virtual_repository;
 
 @RestController
@@ -25,13 +28,23 @@ public class Vd_Cp_Loja_Virt_Controller {
 	private EnderecoRepository enderecoRepository;
 	
 	
+	@Autowired
+	private PessoaController pessoaController;
+	
 	@ResponseBody
 	@PostMapping(value = "**/salvarVendaLoja")
-	public ResponseEntity<VendaCompraLojaVirtual> salvarVendaLoja(@RequestBody @Valid VendaCompraLojaVirtual vendaCompraLojaVirtual) {
+	public ResponseEntity<VendaCompraLojaVirtual> salvarVendaLoja(@RequestBody @Valid VendaCompraLojaVirtual vendaCompraLojaVirtual) throws ExceptionLojaVirtual {
 		
+		
+		
+		PessoaFisica pessoaFisica = pessoaController.salvarPf(vendaCompraLojaVirtual.getPessoa()).getBody();
+		vendaCompraLojaVirtual.setPessoa(pessoaFisica);
+		
+		vendaCompraLojaVirtual.getEnderecoCobranca().setPessoa(pessoaFisica);
 		Endereco enderecoCobranca = enderecoRepository.save(vendaCompraLojaVirtual.getEnderecoCobranca());
 		vendaCompraLojaVirtual.setEnderecoCobranca(enderecoCobranca);
 		
+		vendaCompraLojaVirtual.getEnderecoEntrega().setPessoa(pessoaFisica);
 		Endereco enderecoEntrega = enderecoRepository.save(vendaCompraLojaVirtual.getEnderecoEntrega());
 		vendaCompraLojaVirtual.setEnderecoEntrega(enderecoEntrega);
 		
