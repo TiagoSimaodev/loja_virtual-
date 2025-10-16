6140329b3111ec329e6a26f971dd0f2f34b5e60c
tiago simao <tiagosimaorodri123@gmail.com>
2025-10-16 01:31:00 -0300

    continuando o dto vendecompra

--- src/main/java/br/com/loja/dto/VendaCompraLojaVirtualDTO.java
+++ src/main/java/br/com/loja/dto/VendaCompraLojaVirtualDTO.java
@@ -2,9 +2,21 @@ package br.com.loja.dto;
 
 import java.math.BigDecimal;
 
+import br.com.loja.model.Pessoa;
+
 public class VendaCompraLojaVirtualDTO {
 	
 	private BigDecimal valorTotal;
+	
+	private Pessoa pessoa;
+	
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+	
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
 	
 	public void setValorTotal(BigDecimal valorTotal) {
 		this.valorTotal = valorTotal;
