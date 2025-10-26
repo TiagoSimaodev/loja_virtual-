458fb03f6f4aef6661b69eea193fab9b8993c118
tiago simao <tiagosimaorodri123@gmail.com>
2025-10-26 02:31:48 -0300

    criado o dto para corrigir o bug de recursividade dos dados

---
+++ src/main/java/br/com/loja/dto/ProdutoDTO.java
@@ -0,0 +1,53 @@
+package br.com.loja.dto;
+
+import java.math.BigDecimal;
+
+public class ProdutoDTO {
+
+	private Long id;
+	private String nome;
+	private BigDecimal valorVenda;
+	private String tipoUnidade;
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getValorVenda() {
+		return valorVenda;
+	}
+
+	public void setValorVenda(BigDecimal valorVenda) {
+		this.valorVenda = valorVenda;
+	}
+
+	public String getTipoUnidade() {
+		return tipoUnidade;
+	}
+
+	public void setTipoUnidade(String tipoUnidade) {
+		this.tipoUnidade = tipoUnidade;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
