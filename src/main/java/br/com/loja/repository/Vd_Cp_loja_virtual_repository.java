2b7bc6a19363af27f07ef6e40c567955cc6df5ba
tiago simao <tiagosimaorodri123@gmail.com>
2025-10-29 01:39:58 -0300

    feito o repository para exclusão da venda.

--- src/main/java/br/com/loja/repository/Vd_Cp_loja_virtual_repository.java
+++ src/main/java/br/com/loja/repository/Vd_Cp_loja_virtual_repository.java
@@ -1,6 +1,7 @@
 package br.com.loja.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,6 +11,12 @@ import br.com.loja.model.VendaCompraLojaVirtual;
 @Transactional
 public interface Vd_Cp_loja_virtual_repository extends JpaRepository<VendaCompraLojaVirtual, Long> {
 
+	
+	@Query(value = "select a from VendaCompraLojaVirtual a where a.id = ?1 and a.excluido = false")
+	VendaCompraLojaVirtual findByIdExclusao(Long id); 
+		
+		
+		
 	
 	
 }
