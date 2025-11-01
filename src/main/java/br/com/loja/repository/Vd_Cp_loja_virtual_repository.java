47f00961837adb26aecddc0ba048da3a09b6e13c
tiago simao <tiagosimaorodri123@gmail.com>
2025-11-01 02:14:17 -0300

    criado a query para consultar venda por por nome do cliente.

--- src/main/java/br/com/loja/repository/Vd_Cp_loja_virtual_repository.java
+++ src/main/java/br/com/loja/repository/Vd_Cp_loja_virtual_repository.java
@@ -22,6 +22,9 @@ public interface Vd_Cp_loja_virtual_repository extends JpaRepository<VendaCompra
 
 	@Query(value = "select distinct(i.vendaCompraLojaVirtual) from ItemVendaLoja i where i.vendaCompraLojaVirtual.excluido = false and upper(trim(i.produto.nome)) like %?1% ")
 	List<VendaCompraLojaVirtual> vendaPorNomeProduto(String valor);
+
+	@Query(value = "select distinct(i.vendaCompraLojaVirtual) from ItemVendaLoja i where i.vendaCompraLojaVirtual.excluido = false and upper(trim(i.vendaCompraLojaVirtual.pessoa.nome)) like %?1% ")
+	List<VendaCompraLojaVirtual> vendaPorNomeCliente(String nomepessoa);
 		
 	
 	
