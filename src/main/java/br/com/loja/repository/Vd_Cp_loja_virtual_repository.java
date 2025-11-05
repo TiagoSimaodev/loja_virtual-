5653af21c63f37cbc4b01fc8f55a160fae135fd2
tiago simao <tiagosimaorodri123@gmail.com>
2025-11-05 02:35:20 -0300

    Criado a query de consulta para endereço de entrega.

--- src/main/java/br/com/loja/repository/Vd_Cp_loja_virtual_repository.java
+++ src/main/java/br/com/loja/repository/Vd_Cp_loja_virtual_repository.java
@@ -28,6 +28,9 @@ public interface Vd_Cp_loja_virtual_repository extends JpaRepository<VendaCompra
 
 	@Query(value = "select distinct(i.vendaCompraLojaVirtual) from ItemVendaLoja i where i.vendaCompraLojaVirtual.excluido = false and upper(trim(i.vendaCompraLojaVirtual.enderecoCobranca.ruaLogra)) like %?1% ")
 	List<VendaCompraLojaVirtual> vendaPorEnderecoCobranca(String enderecoCobranca);
+
+	@Query(value = "select distinct(i.vendaCompraLojaVirtual) from ItemVendaLoja i where i.vendaCompraLojaVirtual.excluido = false and upper(trim(i.vendaCompraLojaVirtual.enderecoEntrega.ruaLogra)) like %?1% ")
+	List<VendaCompraLojaVirtual> vendaPorEnderecoEntrega(String enderecoEntrega);
 		
 	
 	
