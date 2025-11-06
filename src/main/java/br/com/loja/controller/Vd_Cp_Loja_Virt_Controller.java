abdb37ff0d23da7000b9182645e55ab5dfd220e6
tiago simao <tiagosimaorodri123@gmail.com>
2025-11-06 02:05:44 -0300

    criado o endpoint para a consulta intervalo de data,

--- src/main/java/br/com/loja/controller/Vd_Cp_Loja_Virt_Controller.java
+++ src/main/java/br/com/loja/controller/Vd_Cp_Loja_Virt_Controller.java
@@ -203,7 +203,63 @@ public class Vd_Cp_Loja_Virt_Controller {
 		return new ResponseEntity<String>("Venda ativada com sucesso!",HttpStatus.OK);
 	}
 	
-	
+	@ResponseBody
+	@GetMapping(value = "**/consultaVendaDinamicaFaixaData/{data1}/{data2}")
+	public ResponseEntity<List<VendaCompraLojaVirtualDTO>> 
+	consultaVendaDinamicaFaixaData(@PathVariable("data1") String data1, @PathVariable("data2") String data2) {
+		
+		List<VendaCompraLojaVirtual> compraLojaVirtual = null;
+		
+		compraLojaVirtual = vd_Cp_loja_virtual_repository.consultaVendaFaixaData(data1, data2);
+		
+		
+		if (compraLojaVirtual == null) {
+			compraLojaVirtual = new ArrayList<VendaCompraLojaVirtual>();
+		}
+		
+			List<VendaCompraLojaVirtualDTO> compraLojaVirtualDTOList = new ArrayList<VendaCompraLojaVirtualDTO>();
+		
+		for (VendaCompraLojaVirtual vel : compraLojaVirtual) {
+			
+		
+
+			VendaCompraLojaVirtualDTO compraLojaVirtualDTO = new VendaCompraLojaVirtualDTO();
+
+			compraLojaVirtualDTO.setValorTotal(vel.getValorTotal());
+			compraLojaVirtualDTO.setPessoa(vel.getPessoa());
+
+			compraLojaVirtualDTO.setCobranca(vel.getEnderecoCobranca());
+			compraLojaVirtualDTO.setEntrega(vel.getEnderecoEntrega());
+			compraLojaVirtualDTO.setValorDesc(vel.getValorDesconto());
+			compraLojaVirtualDTO.setValorFrete(vel.getValorFrete());
+			compraLojaVirtualDTO.setId(vel.getId());
+
+			for (ItemVendaLoja item : vel.getItemVendaLojas()) {
+				ItemVendaLojaDTO itemVendaLojaDTO = new ItemVendaLojaDTO();
+				itemVendaLojaDTO.setQuantidade(item.getQuantidade());
+			
+				ProdutoDTO produtoDTO = new ProdutoDTO();
+			
+				produtoDTO.setId(item.getProduto().getId());
+				produtoDTO.setDescricao(item.getProduto().getDescricao());
+				produtoDTO.setNome(item.getProduto().getNome());
+				produtoDTO.setTipoUnidade(item.getProduto().getTipoUnidade());
+				produtoDTO.setValorVenda(item.getProduto().getValorVenda());
+			
+			
+				itemVendaLojaDTO.setProduto(produtoDTO);
+
+				compraLojaVirtualDTO.getItemVendaLoja().add(itemVendaLojaDTO);
+
+					}
+				compraLojaVirtualDTOList.add(compraLojaVirtualDTO);
+				
+			}
+
+		return new ResponseEntity<List<VendaCompraLojaVirtualDTO>>(compraLojaVirtualDTOList, HttpStatus.OK);
+
+		
+	}
 	
 	
 	@ResponseBody
