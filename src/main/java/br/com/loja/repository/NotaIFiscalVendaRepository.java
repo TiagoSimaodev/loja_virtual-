7e476ee5da5c6b1e8850358442ac9588e70fbc85
tiago simao <tiagosimaorodri123@gmail.com>
2025-12-02 00:36:06 -0300

    criado as query para buscar nota por venda.

--- src/main/java/br/com/loja/repository/NotaIFiscalVendaRepository.java
+++ src/main/java/br/com/loja/repository/NotaIFiscalVendaRepository.java
@@ -1,6 +1,9 @@
 package br.com.loja.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,5 +12,11 @@ import br.com.loja.model.NotaFiscalVenda;
 @Repository
 @Transactional
 public interface NotaIFiscalVendaRepository extends JpaRepository<NotaFiscalVenda, Long> {
-
+	
+	
+	@Query(value = "select n from NotaFiscalVenda n  where n.vendaCompraLojaVirtual.id = ?1")
+	List<NotaFiscalVenda>  buscaNotaPorVenda(Long idVenda);
+	
+	@Query(value = "select n from NotaFiscalVenda n  where n.vendaCompraLojaVirtual.id = ?1")
+	NotaFiscalVenda  buscaNotaPorVendaUnica(Long idVenda);
 }
