0d4a9166d651f53e3b9af45d27ef1eb905924687
tiago simao <tiagosimaorodri123@gmail.com>
2025-12-05 01:28:03 -0300

    criado o crud inicial do Cupom desconto.

--- src/main/java/br/com/loja/controller/CupDescontoController.java
+++ src/main/java/br/com/loja/controller/CupDescontoController.java
@@ -2,14 +2,20 @@ package br.com.loja.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.loja.ExceptionLojaVirtual;
 import br.com.loja.model.CupDesc;
 import br.com.loja.repository.CupDescontoRepository;
 
@@ -18,6 +24,44 @@ public class CupDescontoController {
 
 	@Autowired
 	private CupDescontoRepository cupDescontoRepository;
+	
+	
+	
+	@ResponseBody
+	@GetMapping(value = "**/obterCumpoDescPor/{id}")
+	public ResponseEntity<CupDesc> obterCumpoDescPorId(@PathVariable("id") Long id) throws ExceptionLojaVirtual {
+		
+		CupDesc cupDesc = cupDescontoRepository.findById(id).orElse(null);
+		
+		if(cupDesc == null) {
+			throw new ExceptionLojaVirtual("Não encontrou Cupom desconto com o codigo" + id);
+			
+		}
+		
+		return new ResponseEntity<CupDesc>(cupDesc, HttpStatus.OK);
+		
+	}
+	
+	@ResponseBody
+	@DeleteMapping(value = "**/deleteCupomPorId/{id}")
+	public ResponseEntity<?> deleteCupomPorId(@PathVariable("id") Long id) {
+		
+		 cupDescontoRepository.deleteById(id);
+		
+		return new ResponseEntity("Cupom Produto Removio", HttpStatus.OK);
+		
+	}
+	
+	
+	@ResponseBody
+	@PostMapping(value ="**/salvarCupDesc")
+	public ResponseEntity<CupDesc> salvarCupDesc(@RequestBody @Valid CupDesc cupDesc){
+		
+		CupDesc cupDesc2 = cupDescontoRepository.save(cupDesc);
+		
+		return new ResponseEntity<CupDesc>(cupDesc2, HttpStatus.CREATED);
+		
+	}
 	
 	
 	@ResponseBody
