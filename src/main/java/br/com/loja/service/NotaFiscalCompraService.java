a9d172d5cabde692696fe506044e2c96b146c008
tiago simao <tiagosimaorodri123@gmail.com>
2025-12-19 02:22:16 -0300

    feito as anotações para não permitir que o campo data vá limpo

--- src/main/java/br/com/loja/service/NotaFiscalCompraService.java
+++ src/main/java/br/com/loja/service/NotaFiscalCompraService.java
@@ -21,11 +21,20 @@ public class NotaFiscalCompraService {
 
 		List<ObjetoRequisicaoRelatorioProdCompraNotaFiscalDTO> retorno = new ArrayList<ObjetoRequisicaoRelatorioProdCompraNotaFiscalDTO>();
 		
-		String sql = "";
+		String sql = "select p.id as codigoProduto, p.nome as nomeProduto, "
+				+ " p.valor_venda as valorVendaProduto, ntp.quantidade as quantidadeComprada, "
+				+ " pj.id as codigoFornecedor, pj.nome as nomeFornecedor,cfc.data_compra as dataCompra "
+				+ " from nota_fiscal_compra as cfc "
+				+ " inner join nota_item_produto as ntp on cfc.id = nota_fiscal_compra_id  "
+				+ " inner join produto as p on p.id = ntp.produto_id "
+				+ " inner join pessoa_juridica as pj on pj.id = cfc.pessoa_id where  ";
 		
+		
+		sql += " cfc.data_compra >='"+objetoRequisicaoRelatorioProdCompraNotaFiscaldto.getDataInicial()+"' and  ";
+		sql += " cfc.data_compra <= '"+objetoRequisicaoRelatorioProdCompraNotaFiscaldto.getDataFinal()+"' ";
 		retorno = jdbcTemplate.query(sql, new BeanPropertyRowMapper(ObjetoRequisicaoRelatorioProdCompraNotaFiscalDTO.class));
 		
-		return null;
+		return retorno;
 	}
 
 }
