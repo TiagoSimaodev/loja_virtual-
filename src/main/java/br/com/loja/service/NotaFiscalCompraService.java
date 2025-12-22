f6c3025170943deae690bbd2e8923d2d2923ace9
tiago simao <tiagosimaorodri123@gmail.com>
2025-12-22 01:59:01 -0300

    feito a regra de negocio, e o sql para buscar no banco, e as validações.

--- src/main/java/br/com/loja/service/NotaFiscalCompraService.java
+++ src/main/java/br/com/loja/service/NotaFiscalCompraService.java
@@ -8,6 +8,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import br.com.loja.dto.ObjetoRelatorioStatusCompraDTO;
 import br.com.loja.dto.ObjetoRequisicaoRelatorioProdAlertaEstoqueDTO;
 import br.com.loja.dto.ObjetoRequisicaoRelatorioProdCompraNotaFiscalDTO;
 
@@ -16,6 +17,50 @@ public class NotaFiscalCompraService {
 	
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+	
+	
+	
+	
+	public List<ObjetoRelatorioStatusCompraDTO> relatorioStatusVendaLojaVirtual(ObjetoRelatorioStatusCompraDTO objetoRelatorioStatusCompraDTO) {
+			
+		List<ObjetoRelatorioStatusCompraDTO> retorno = new ArrayList<ObjetoRelatorioStatusCompraDTO>();
+
+		String sql = "select p.id as codigoProduto, "
+				+ " p.nome as nomeProduto, "
+				+ " pf.email as emailCliente, "
+				+ " pf.telefone as foneCliente, "
+				+ " p.valor_venda as valorVendaProduto, "
+				+ " pf.id as codigoCliente, "
+				+ " pf.nome as nomeCliente, "
+				+ " p.qtd_estoque as qtdEstoque, "
+				+ " cfc.id as codigoVenda, "
+				+ " cfc.status_venda_loja_virtual as statusVenda "
+				+ " from vd_cp_loja_virt as cfc "
+				+ " inner join item_venda_loja as ntp on ntp.venda_compra_loja_virt_id = cfc.id "
+				+ " inner join produto as p on p.id = ntp.produto_id "
+				+ " inner join pessoa_fisica as pf on pf.id = cfc.pessoa_id ";
+		
+		
+				sql += " where cfc.data_venda >= '"+objetoRelatorioStatusCompraDTO.getDataInicial()+"' and cfc.data_venda <= '"+objetoRelatorioStatusCompraDTO.getDataFinal()+"' ";
+				
+				if(!objetoRelatorioStatusCompraDTO.getNomeProduto().isEmpty()) {
+					sql += " and upper(p.nome) like upper('%"+objetoRelatorioStatusCompraDTO.getNomeProduto()+"%') ";
+				}
+				
+				if(!objetoRelatorioStatusCompraDTO.getStatusVenda().isEmpty()) {
+				
+					sql	+= " and cfc.status_venda_loja_virtual in ('"+objetoRelatorioStatusCompraDTO.getStatusVenda()+"') ";
+				}
+				
+				if(objetoRelatorioStatusCompraDTO.getNomeCliente().isEmpty()) {
+					sql +="and pf.nome like '%"+objetoRelatorioStatusCompraDTO.getNomeCliente()+"%' ";
+				}
+				
+		retorno = jdbcTemplate.query(sql, new BeanPropertyRowMapper(ObjetoRelatorioStatusCompraDTO.class));
+				
+		return retorno;
+	}
+	
 	
 	
 	/** 
