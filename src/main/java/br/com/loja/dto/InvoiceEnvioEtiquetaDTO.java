a470366dd0ea16d8b8a7978bede51653bfef100b
tiago simao <tiagosimaorodri123@gmail.com>
2026-01-20 01:06:52 -0300

    criado o invoice do envio da etiqueta

---
+++ src/main/java/br/com/loja/dto/InvoiceEnvioEtiquetaDTO.java
@@ -0,0 +1,23 @@
+package br.com.loja.dto;
+
+import java.io.Serializable;
+
+public class InvoiceEnvioEtiquetaDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	
+	private String key;
+
+
+	public String getKey() {
+		return key;
+	}
+
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+	
+	
+}
