2757862ccb553d29bf33b16026b30e72dd8324f3
tiago simao <tiagosimaorodri123@gmail.com>
2026-01-21 00:39:03 -0300

    alteração feita no dto envio etiqueta

--- src/main/java/br/com/loja/dto/EnvioEtiquetaDTO.java
+++ src/main/java/br/com/loja/dto/EnvioEtiquetaDTO.java
@@ -9,7 +9,6 @@ public class EnvioEtiquetaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private String service;
-	
 	private String agency;
 	
 	private FromEnvioEtiquetaDTO from = new FromEnvioEtiquetaDTO();
